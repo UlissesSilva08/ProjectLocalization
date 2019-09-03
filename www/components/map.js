339f984f4e094a2a38c5007793a7f6c856f765d3
RichardKauan <richardkauan0@gmail.com>
2019-09-04 02:26:49 +0900

    Mapa funcionando com localização atual

--- www/components/map.js
+++ www/components/map.js
@@ -1,2 +1,31 @@
-// This is a JavaScript file
+$(document).on("click","#local",function(){
 
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.NONE]     = 'No network connection';
+}
+
+checkConnection();
+
+var onSuccess = function(position) {
+
+L.mapquest.key = 'V75qxvRFj6ZCGWQ0RSR4lzhK7NR3Fs4H';
+
+// 'map' refers to a <div> element with the ID map
+L.mapquest.map('map', {
+  center: [position.coords.latitude, position.coords.longitude],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 15,
+});
+};
+
+function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+});
